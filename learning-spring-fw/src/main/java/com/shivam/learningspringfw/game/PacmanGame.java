326572b52ae74782bce69242d88ac7d235e0a011
Shivam97.rs@gmail.com <crikfan5@gmail.com>
2023-09-14 12:06:34 -0400

    Changes upto ITR2

---
+++ learning-spring-fw/src/main/java/com/shivam/learningspringfw/game/PacmanGame.java
@@ -0,0 +1,24 @@
+package com.shivam.learningspringfw.game;
+
+public class PacmanGame implements GamingConsole {
+
+	@Override
+	public void up() {
+		System.out.println("Jump");
+	}
+
+	@Override
+	public void down() {
+		System.out.println("Hide");
+	}
+
+	@Override
+	public void left() {
+		System.out.println("Do something");
+	}
+
+	@Override
+	public void right() {
+		System.out.println("Walk");
+	}
+}
