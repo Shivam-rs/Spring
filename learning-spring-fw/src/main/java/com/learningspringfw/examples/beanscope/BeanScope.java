4690fe9241fe46405d5b62c1e1db9fc139df42f4
Shivam97.rs@gmail.com <crikfan5@gmail.com>
2023-09-19 23:25:47 -0400

    Uptill Bean Scope

---
+++ learning-spring-fw/src/main/java/com/learningspringfw/examples/beanscope/BeanScope.java
@@ -0,0 +1,43 @@
+package com.learningspringfw.examples.beanscope;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+class NormalClass {
+
+}
+
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Component
+class PrototypeClass {
+
+}
+
+@Configuration
+@ComponentScan
+public class BeanScope {
+
+	public static void main(String[] args) {
+
+		try (var context = new AnnotationConfigApplicationContext(BeanScope.class)) {
+
+			System.out.println(context.getBean(NormalClass.class));
+			System.out.println(context.getBean(NormalClass.class));
+			System.out.println(context.getBean(NormalClass.class));
+			System.out.println(context.getBean(NormalClass.class));
+
+			System.out.println(context.getBean(PrototypeClass.class));
+			System.out.println(context.getBean(PrototypeClass.class));
+			System.out.println(context.getBean(PrototypeClass.class));
+			System.out.println(context.getBean(PrototypeClass.class));
+
+		}
+
+	}
+
+}
--- learning-spring-fw/src/main/java/com/learningspringfw/gamelaunch/GameLauncherBasic.java
+++ learning-spring-fw/src/main/java/com/learningspringfw/gamelaunch/GameLauncherBasic.java
@@ -1,17 +1,15 @@
 package com.learningspringfw.gamelaunch;
 
 import com.learningspringfw.games.GameRunner;
-import com.learningspringfw.games.MarioGame;
 import com.learningspringfw.games.PacmanGame;
-import com.learningspringfw.games.SuperContraGame;
 
 public class GameLauncherBasic {
 
 	public static void main(String[] args) {
 
-		var marioGame = new MarioGame();
+		// var marioGame = new MarioGame();
 
-		var superContraGame = new SuperContraGame();
+		// var superContraGame = new SuperContraGame();
 
 		var pacmanGame = new PacmanGame();
 		var gameRunner = new GameRunner(pacmanGame);
