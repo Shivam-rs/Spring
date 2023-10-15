b3e0bc34981ecf7ec0552edc9a2267a3f025edea
Shivam97.rs@gmail.com <crikfan5@gmail.com>
2023-10-15 18:32:11 -0400

    Spring Boot - How to manage profile

---
+++ mastering-spring-fw/src/main/java/com/masteringspringfw/springboot/CourseController.java
@@ -0,0 +1,18 @@
+package com.masteringspringfw.springboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+
+	@RequestMapping("/courses") // http://localhost:8080/courses
+	public List<Course> retriveAllCourses() {
+		return Arrays.asList(new Course(1, "Spring", "Ranga"), new Course(2, "Spring Boot", "Ranga"),
+				new Course(3, "Maven", "Ranga"), new Course(4, "AWS", "Ranga"));
+	}
+
+}
---
+++ mastering-spring-fw/src/main/java/com/masteringspringfw/springboot/LearnSpringBootApp.java
@@ -0,0 +1,14 @@
+package com.masteringspringfw.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootApp.class, args);
+
+	}
+
+}
